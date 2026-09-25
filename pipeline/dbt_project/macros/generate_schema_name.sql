04e2fee46606cc6149eaac3586dac6d731bcc3c0
Jason M Netherland <JMNetherland@outlook.com>
2026-09-25 08:49:24 -0500

    Initial commit: 7-repo open-source data stack (homelab edition)

---
+++ pipeline/dbt_project/macros/generate_schema_name.sql
@@ -0,0 +1,4 @@
+{# Use the schema exactly as configured (staging / marts), not <target>_<schema>. #}
+{% macro generate_schema_name(custom_schema_name, node) -%}
+    {{ custom_schema_name if custom_schema_name is not none else target.schema }}
+{%- endmacro %}
